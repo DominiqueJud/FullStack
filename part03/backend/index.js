f5340cab6c39dabbb982e21bef7f8dc21b56c4f5
DominiqueJud <161980298+DominiqueJud@users.noreply.github.com>
2024-06-26 10:24:00 +0200

    BackendUpdate
    implemented put, delete and /info functionality

--- part03/backend/index.js
+++ part03/backend/index.js
@@ -6,8 +6,6 @@ app.use(express.json())
 app.use(express.static("dist"))
 
 
-const Person=require("./modules/person.js")
-
 
 morgan.token("reqData",(req,res)=>{
     console.log(req.body)
@@ -16,29 +14,6 @@ morgan.token("reqData",(req,res)=>{
 })
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :reqData"))
-
-let persons=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 
 
@@ -54,25 +29,25 @@ app.get("/api/persons", (request, response)=>{
 })
 
 app.get("/api/persons/:id",(request,response)=>{
-    const id= Number(request.params.id)
-    const person=Person.find(person=> person.id===id)
-    console.log(person, id)
-    if(person){
-        response.json(person)
-    }
-    else{
-        response.status(404).json({error:"id not found"})
-        console.log("person not found")
-    }
-
-
+    Person.findById(request.params.id)
+    .then(person=>{
+        if(person){
+            response.json(person)
+        }
+        else{
+            response.status(404).end()
+        }
+    })
+    .catch(error =>next(error))
 })
 
 
 app.get("/info", (request, response)=>{
     const date= new Date()
+    Person.find({})
+    .then(persons=>{
     response.send(`<p>Phonebook has info for ${persons.length} persons<br/>
-        ${date}</p>`)
+        ${date}</p>`)})
 })
 
 app.post("/api/persons", (request,response)=>{
@@ -98,17 +73,25 @@ app.post("/api/persons", (request,response)=>{
     })
 })
 
-app.delete("/api/persons/:id", (request,response)=>{
-    const id=Number(request.params.id)
-    const person=persons.find(person=>person.id==id)
-    if(person){
-        persons=persons.filter(person => person.id !==id)
-        response.status(204).end()
+app.delete("/api/persons/:id", (request,response, next)=>{
+    Person.findByIdAndDelete(request.params.id)
+    .then(person=>{
+        response.status(204).end()}
+    )
+    .catch(error=>next(error))
+})
+
+app.put("/api/persons/:id", (request,response,next)=>{
+    const body=request.body
+    const person={
+        name:body.name,
+        number:body.number,
     }
-    else{
-        response.status(204).end()
-        console.log("no user to delete with this id")
-    }
+    Person.findByIdAndUpdate(request.params.id,person,{new:true})
+    .then(updatedPerson=>{
+        response.json(updatedPerson)
+    })
+    .catch(error=>next(error))
 })
 
 const unknownEndpoint=(request, response)=>{
@@ -116,6 +99,19 @@ const unknownEndpoint=(request, response)=>{
 }
 
 app.use(unknownEndpoint)
+
+
+const errorHandler=(error,response,request,next)=>{
+    console.log(error.message)
+    if(error.name==="CastError"){
+        return response.status(400).send({error:"malformatted id"})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
+const Person=require("./modules/person.js")
+
 
 const PORT= process.env.PORT||3001 
 app.listen(PORT)
