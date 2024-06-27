8b936480f1d004e132105e0c868f5fb2a2b769c9
DominiqueJud <161980298+DominiqueJud@users.noreply.github.com>
2024-06-27 10:38:20 +0200

    Finished Part 03
    Finished part 03

--- part03/backend/modules/person.js
+++ part03/backend/modules/person.js
@@ -1,41 +1,43 @@
-const MONGODB_URL=process.env.MONGODB_URL
-const mongoose=require("mongoose")
+const MONGODB_URL = process.env.MONGODB_URL
+const mongoose = require('mongoose')
 
-personSchema=mongoose.Schema({
-    name:{type:String,
-        minLength: 3,
-        required:true,
+const personSchema = mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (value) => {
+        return /^\d{2,3}[-]d*/.test(value)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number:{type:String,
-        minLength:8,
-        validate:{
-            validator:(value)=>{
-                return /^\d{2,3}[-]d*/.test(value)
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        required:[true,"User phone Number required"],
-    },
+    required: [true, 'User phone Number required'],
+  },
 })
 
-personSchema.set("toJSON",{
-    transform: (document, returnedObject)=>{
-        returnedObject.id=returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject._v
-    }
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject._v
+  }
 })
 
-mongoose.set("strictQuery", false)
+mongoose.set('strictQuery', false)
 
-console.log("connecting to MongoDB")
+console.log('connecting to MongoDB')
 mongoose.connect(MONGODB_URL)
-.then(result=>{
-    console.log("connection to MongoDB established")
-})
-.catch(error=>{
-    console.log("error connectiong to MongoDB", error.message)
-})
+  .then(result => {
+    console.log('connection to MongoDB established')
+  })
+  .catch(error => {
+    console.log('error connectiong to MongoDB', error.message)
+  })
 
 
-module.exports= mongoose.model("Person",personSchema)
+module.exports = mongoose.model('Person', personSchema)
